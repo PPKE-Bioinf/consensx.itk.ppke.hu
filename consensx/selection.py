8ee2403065dd01b75a87e8d8b0dba0a175d7e4b6
puinenveturi <daniel.dudola@gmail.com>
2017-07-20 18:25:44 +0200

    remove: debug log

--- consensx/selection.py
+++ consensx/selection.py
@@ -665,7 +665,9 @@ def run_selection(my_path, original_values, user_selection_JSON):
     in_selection, iter_data, iter_all = selection_on(
         working_dir, measure, user_sel,
         min_size=min_size, max_size=max_size, overdrive=overdrive
-        )
+    )
+
+    print("ITER ALL", iter_all)
 
     # sel_calced = open(my_path + "/selection_calced.txt", 'w')
 
