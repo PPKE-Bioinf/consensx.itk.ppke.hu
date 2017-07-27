3aa7c79ebc83de210c0ad7b6a28f7631e1709b51
puinenveturi <daniel.dudola@gmail.com>
2017-07-27 19:18:30 +0200

    add: postgre SQL database

--- csx_unchained/settings.py
+++ csx_unchained/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'csx_unchained.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'consensx',
+        'USER': 'consensxuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
