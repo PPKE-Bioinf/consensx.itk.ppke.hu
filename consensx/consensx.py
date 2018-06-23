51ee91e77f14fb98ec566e0336f8418d7c6d682b
puinenveturi <daniel.dudola@gmail.com>
2018-06-23 17:50:09 +0200

    refactor + enables peptide bond angles calculation

--- consensx/consensx.py
+++ consensx/consensx.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 """
-This is the Django webapp implemetation of
+This is the Django webapp implementation of
               _____      _   _  _____ ______          __   __
              / ____|    | \ | |/ ____|  ____|         \ \ / /
             | |     ___ |  \| | (___ | |__   _ __  ___ \ V /
@@ -41,23 +41,23 @@ def run_calculation(request, calc_id):
 
     from django.conf import settings
     my_path = os.path.join(settings.MEDIA_ROOT, my_id) + '/'
-    DB_entry = CSX_upload.objects.get(id_code=calc_id)
+    db_entry = CSX_upload.objects.get(id_code=calc_id)
 
     # ----------------  Setting up working directory and files  ------------- #
-    my_CSV_buffer = csx_obj.CSV_buffer(my_path)
+    my_csv_buffer = csx_obj.CSV_buffer(my_path)
 
-    my_PDB = my_path + DB_entry.PDB_file
+    my_pdb = my_path + db_entry.PDB_file
 
-    csx_func.pdb_cleaner(my_path, my_PDB, my_CSV_buffer)
-    model_count = csx_func.pdb_splitter(my_path, my_PDB)
+    csx_func.pdb_cleaner(my_path, my_pdb, my_csv_buffer)
+    model_count = csx_func.pdb_splitter(my_path, my_pdb)
 
-    if not csx_func.get_model_list(my_PDB, my_path, model_count):
+    if not csx_func.get_model_list(my_pdb, my_path, model_count):
         return render(request, "consensx/home.html", {
             "error": "DISCARDED MODELS FOUND, CHECK IF ALL MODELS HAVE THE SAME\
             NUMBER OF ATOMS"
         })
 
-    pdb_models = []                                # list of models (PDB)
+    pdb_models = []
     for file in os.listdir(my_path):
         if file.startswith("model_") and file.endswith(".pdb"):
             pdb_models.append(file)
@@ -66,91 +66,91 @@ def run_calculation(request, calc_id):
     data_found = False
 
     # ---------------------  Read  and parse NOE file   --------------------- #
-    if DB_entry.NOE_file:
+    if db_entry.NOE_file:
         # empty class variables
         csx_obj.Restraint_Record.all_restraints = []
         csx_obj.Restraint_Record.resolved_restraints = []
 
-        NOE_name = DB_entry.NOE_file
-        my_NOE = my_path + DB_entry.NOE_file
-        saveShifts = csx_func.getNOE(my_NOE)
-        NOE_n = saveShifts[-1][0] + " distance restraints found"
-        NOE_violations = csx_calc.calcNOEviolations(
-            my_PDB, saveShifts, my_path, DB_entry.r3average
+        noe_name = db_entry.NOE_file
+        my_noe = my_path + db_entry.NOE_file
+        save_shifts = csx_func.getNOE(my_noe)
+        noe_n = save_shifts[-1][0] + " distance restraints found"
+        noe_violations = csx_calc.calcNOEviolations(
+            my_pdb, save_shifts, my_path, db_entry.r3average
         )
-        PRIDE_data = csx_calc.calcNMR_Pride(pdb_models, my_path)
+        pride_data = csx_calc.calcNMR_Pride(pdb_models, my_path)
 
-        NOE_PRIDE_data = {
-            "NOE_violations": str(NOE_violations),
+        noe_pride_data = {
+            "noe_violations": str(noe_violations),
             "NOE_hist": my_id + "/NOE_hist.svg",
-            "best_score": PRIDE_data[0],
-            "worst_score": PRIDE_data[1],
-            "average_score": '{0:.3f}'.format(PRIDE_data[2]),
-            "deviation": '{0:.3f}'.format(PRIDE_data[3]),
+            "best_score": pride_data[0],
+            "worst_score": pride_data[1],
+            "average_score": '{0:.3f}'.format(pride_data[2]),
+            "deviation": '{0:.3f}'.format(pride_data[3]),
             "PRIDE_hist": my_id + "/PRIDE-NMR_score.svg"
         }
         data_found = True
     else:
-        NOE_name = "[NOT PRESENT]"
-        NOE_n = ""
-        NOE_PRIDE_data = None
+        noe_name = "[NOT PRESENT]"
+        noe_n = ""
+        noe_pride_data = None
 
     # ---------------------  Read  and parse STR file   --------------------- #
-    if DB_entry.STR_file:
-        STR_name = DB_entry.STR_file
-        my_STR = my_path + DB_entry.STR_file
+    if db_entry.STR_file:
+        str_name = db_entry.STR_file
+        my_str = my_path + db_entry.STR_file
 
         try:
-            parsed = csx_func.parseSTR(my_STR)
+            parsed = csx_func.parseSTR(my_str)
         except Exception as e:
             print("EXCEPTION", e)
             return HttpResponse(e)
 
         # ------------------------  RDC calculation  ------------------------ #
-        RDC_lists = csx_func.get_RDC_lists(parsed.value)
-        RDC_lists_path = my_path + "/RDC_lists.pickle"
-        pickle.dump(RDC_lists, open(RDC_lists_path, "wb"))
+        rdc_lists = csx_func.get_RDC_lists(parsed.value)
+        rdc_lists_path = my_path + "/RDC_lists.pickle"
+        pickle.dump(rdc_lists, open(rdc_lists_path, "wb"))
 
-        if RDC_lists:
-            SVD_enabled = DB_entry.svd_enable
-            lc_model = DB_entry.rdc_lc
-            RDC_data = csx_calc.calcRDC(my_CSV_buffer, RDC_lists, pdb_models,
-                                        my_path, SVD_enabled, lc_model)
+        if rdc_lists:
+            svd_enabled = db_entry.svd_enable
+            lc_model = db_entry.rdc_lc
+            rdc_data = csx_calc.calcRDC(my_csv_buffer, rdc_lists, pdb_models,
+                                        my_path, svd_enabled, lc_model)
             data_found = True
         else:
-            RDC_data = None
+            rdc_data = None
 
         # ----------------------------  S2 calc  ---------------------------- #
-        S2_dict = csx_func.parseS2_STR(parsed.value)
-        S2_dump = [S2_dict, DB_entry.superimpose, DB_entry.fit_range]
-        S2_dict_path = my_path + "/S2_dict.pickle"
-        pickle.dump(S2_dump, open(S2_dict_path, "wb"))
+        s2_dict = csx_func.parseS2_STR(parsed.value)
+        s2_dump = [s2_dict, db_entry.superimpose, db_entry.fit_range]
+        s2_dict_path = my_path + "/S2_dict.pickle"
+        pickle.dump(s2_dump, open(s2_dict_path, "wb"))
 
-        if S2_dict:
-            S2_data = csx_calc.calcS2(
-                           my_CSV_buffer, S2_dict, my_path,
-                           fit=DB_entry.superimpose,
-                           fit_range=DB_entry.fit_range)
+        if s2_dict:
+            s2_data = csx_calc.calcS2(
+                           my_csv_buffer, s2_dict, my_path,
+                           fit=db_entry.superimpose,
+                           fit_range=db_entry.fit_range)
 
             data_found = True
         else:
-            S2_data = None
+            s2_data = None
 
-        S2_sidechain = csx_func.parse_sidechain_S2_STR(parsed.value)
+        s2_sidechain = csx_func.parse_sidechain_S2_STR(parsed.value)
 
-        if S2_sidechain:
-            S2_sc_data = csx_calc.calcS2_sidechain(
-                my_CSV_buffer, S2_sidechain, my_path, fit=DB_entry.superimpose
+        if s2_sidechain:
+            s2_sc_data = csx_calc.calcS2_sidechain(
+                my_csv_buffer, s2_sidechain, my_path, fit=db_entry.superimpose
             )
 
-            if "error" in S2_sc_data.keys():
+            if "error" in s2_sc_data.keys():
                 return render(request, "consensx/home.html", {
-                    "error": S2_sidechain["error"]
+                    "error": s2_sidechain["error"]
                 })
 
             data_found = True
         else:
-            S2_sc_data = None
+            s2_sc_data = None
 
         # ------------------------  J-coupling calc  ------------------------ #
         Jcoup_dict = csx_func.parseJcoup_STR(parsed.value)
@@ -158,34 +158,37 @@ def run_calculation(request, calc_id):
         pickle.dump(Jcoup_dict, open(Jcoup_dict_path, "wb"))
 
         if Jcoup_dict:
-            Jcoup_data = csx_calc.calcJCouplings(
-                my_CSV_buffer, DB_entry.karplus, Jcoup_dict, my_PDB, my_path
+            jcoup_data = csx_calc.calcJCouplings(
+                my_csv_buffer, db_entry.karplus, Jcoup_dict, my_pdb, my_path
             )
             data_found = True
         else:
-            Jcoup_data = None
+            jcoup_data = None
 
         # -----------------------  Chemical shift calc  --------------------- #
-        ChemShift_lists = csx_func.parseChemShift_STR(parsed.value)
-        ChemShift_lists_path = my_path + "/ChemShift_lists.pickle"
-        pickle.dump(ChemShift_lists, open(ChemShift_lists_path, "wb"))
+        chem_shift_lists = csx_func.parseChemShift_STR(parsed.value)
+        chem_shift_lists_path = my_path + "/ChemShift_lists.pickle"
+        pickle.dump(chem_shift_lists, open(chem_shift_lists_path, "wb"))
 
-        if ChemShift_lists:
+        if chem_shift_lists:
             chemshift_data = csx_calc.calcChemShifts(
-                my_CSV_buffer, ChemShift_lists, pdb_models, my_path
+                my_csv_buffer, chem_shift_lists, pdb_models, my_path
             )
             data_found = True
         else:
             chemshift_data = None
     else:
-        STR_name = "[NOT PRESENT]"
-        RDC_data = None
-        S2_data = None
-        S2_sc_data = None
-        Jcoup_data = None
+        str_name = "[NOT PRESENT]"
+        rdc_data = None
+        s2_data = None
+        s2_sc_data = None
+        jcoup_data = None
         chemshift_data = None
 
-    my_CSV_buffer.writeCSV()
+    my_csv_buffer.writeCSV()
+
+    csx_func.calcPeptideBonds()
+    csx_func.calcNH_Angles()
 
     if data_found:
         print(csx_obj.CalcPickle.data)
@@ -193,18 +196,18 @@ def run_calculation(request, calc_id):
         pickle.dump(csx_obj.CalcPickle.data, open(calced_values, "wb"))
         rendered_page = render(request, "consensx/calculation.html", {
             "my_id": my_id,
-            "my_PDB": DB_entry.PDB_file,
+            "my_pdb": db_entry.PDB_file,
             "n_model": model_count,
-            "my_NOE": NOE_name,
-            "n_NOE": NOE_n,
-            "my_STR": STR_name,
-            "NOE_PRIDE_data": NOE_PRIDE_data,
-            "RDC_data": RDC_data,
-            "S2_data": S2_data,
-            "S2_sc_data": S2_sc_data,
-            "Jcoup_data": Jcoup_data,
+            "my_NOE": noe_name,
+            "n_NOE": noe_n,
+            "my_STR": str_name,
+            "NOE_PRIDE_data": noe_pride_data,
+            "RDC_data": rdc_data,
+            "S2_data": s2_data,
+            "S2_sc_data": s2_sc_data,
+            "Jcoup_data": jcoup_data,
             "chemshift_data": chemshift_data,
-            "SVD_calc": DB_entry.svd_enable
+            "SVD_calc": db_entry.svd_enable
         })
 
         print("RENDERED PAGE ---------------- START")
