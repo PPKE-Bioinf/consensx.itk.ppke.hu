21dbd9b36924f5ae31275b48a02275393e6a5652
puinenveturi <daniel.dudola@gmail.com>
2018-11-20 14:36:05 +0100

    waits until postgre is accepting connections

--- docker_cmd.sh
+++ docker_cmd.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#waiting for postgres
+until pg_isready --host=consensx-db --username=consensxuser --dbname=consensx
+do
+  echo "Waiting for PostgreSQL..."
+  sleep 1
+done
+
+echo "Postgres is ready, running the migrations..."
+
 python3 manage.py migrate
 echo "Database migration complete."
 echo "Starting CoNSEnsX..."
