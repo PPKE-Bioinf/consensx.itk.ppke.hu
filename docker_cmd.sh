d27968c52c1486eea212517cc87f8d2867142281
derPuntigamer <daniel.dudola@gmail.com>
2018-12-17 14:46:05 +0100

    fixes migration in pipenv run

--- docker_cmd.sh
+++ docker_cmd.sh
@@ -9,7 +9,8 @@ done
 
 echo "Postgres is ready, running the migrations..."
 
-python3 manage.py migrate
+pipenv run python manage.py makemigrations consensx
+pipenv run python manage.py migrate
 echo "Database migration complete."
 echo "Starting CoNSEnsX..."
-python3 manage.py runserver 0.0.0.0:8000
+pipenv run python manage.py runserver 0.0.0.0:8000
