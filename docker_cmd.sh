8aee7039ca758f8ccaa2dd250fabde42a2a8e33e
Daniel Dudola <daniel.dudola@gmail.com>
2019-01-18 16:29:55 +0100

    fixes noreload parameter

--- docker_cmd.sh
+++ docker_cmd.sh
@@ -13,4 +13,4 @@ python3 manage.py makemigrations consensx
 python3 manage.py migrate
 echo "Database migration complete."
 echo "Starting CoNSEnsX..."
-python3 manage.py runserver 0.0.0.0:8000 --no-reload
+python3 manage.py runserver 0.0.0.0:8000 --noreload
