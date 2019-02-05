58424dbaefdaece7834473593e82d9bc1e04202f
derPuntigamer <daniel.dudola@gmail.com>
2019-02-05 17:51:28 +0100

    moves pales call to module

--- consensx/calc/rdc.py
+++ consensx/calc/rdc.py
@@ -1,11 +1,21 @@
 import os
+import sys
 import pickle
 import re
+import subprocess
 
 import consensx.graph as graph
 
 from consensx.csx_libs import methods as csx_func
 from consensx.csx_libs import objects as csx_obj
+
+
+shortcodes = {
+    'ALA': 'A', 'ASP': 'D', 'ASN': 'N', 'ARG': 'R', 'CYS': 'C', 'GLY': 'G',
+    'GLU': 'E', 'GLN': 'Q', 'HIS': 'H', 'ILE': 'I', 'LEU': 'L', 'LYS': 'K',
+    'MET': 'M', 'PHE': 'F', 'PRO': 'P', 'SER': 'S', 'THR': 'T', 'TRP': 'W',
+    'TYR': 'Y', 'VAL': 'V'
+}
 
 
 class RDC_model_data():
@@ -19,6 +29,127 @@ class RDC_model_data():
         else:
             self.rdc_data[RDC_list_num] = {}
             self.rdc_data[RDC_list_num][RDC_type] = RDC_list_data
+
+
+def callPalesOn(my_path, pdb_files, RDC_dict, lc_model, SVD_enable):
+    """Writes pales dummy from the given RDC values, and call Pales with the
+    given parameters"""
+
+    pwd = os.getcwd()
+    os.chdir(my_path)
+
+    for o, pdb_file in enumerate(pdb_files):
+        # ------------------  Open file and read PDB data  -------------------#
+        try:
+            input_pdb = open(pdb_file)
+        except IOError:
+            print("Input file " + pdb_file + " was not found")
+            raise SystemExit                    # exit if input file not found
+
+        seg = []                                # list storing sequence data
+
+        for line in input_pdb:
+            if line.startswith("ATOM") and line.split()[2] == "CA":
+                resname = line.split()[3]       # get residue name
+                seg.append(resname)             # append new segname to list
+
+        input_pdb.close()
+
+        # ----------------------  Write sequence data  -----------------------#
+        short_seg = ""
+
+        for i in range(len(seg)):
+            short_seg += shortcodes[seg[i]]
+
+        my_line = "DATA SEQUENCE "
+        char_counter = 0
+        row_counter = 0
+        pales_dummy = open('pales_dummy.txt', 'w')
+
+        for char in short_seg:
+            if char_counter == 10:          # write aa output in 10 wide blocks
+                my_line += " "
+                char_counter = 0
+                row_counter += 1
+
+                if row_counter == 5:        # write 5 block per line
+                    pales_dummy.write(my_line + "\n")
+                    char_counter = 0
+                    row_counter = 0
+                    my_line = "DATA SEQUENCE "
+
+            my_line += char
+            char_counter += 1
+
+        pales_dummy.write(my_line + "\n")    # write last line of aa output
+
+        # ----------------------  Write dummy dipoles  -----------------------#
+        pales_dummy.write(
+            "\nVARS RESID_I RESNAME_I ATOMNAME_I " +
+            "RESID_J RESNAME_J ATOMNAME_J D DD W\n" +
+            "FORMAT %5d  %6s  %6s  %5d  %6s  %6s  %9.3f  %9.3f  %.2f \n\n"
+        )
+
+        lists = []
+        for RDC_list in list(RDC_dict.keys()):
+            lists.append(RDC_dict[RDC_list])
+
+        for RDC_set in lists:
+            for RDC_record in RDC_set:
+
+                pales_dummy.write(
+                    "%5s  %6s  %6s  %5s  %6s  %6s  %9.3f  %9.3f  %.2f\n" % (
+                        str(RDC_record.resnum) + 'A',
+                        seg[RDC_record.resnum - 1],
+                        str(RDC_record.atom),
+                        str(RDC_record.resnum2) + 'A',
+                        seg[RDC_record.resnum2 - 1],
+                        str(RDC_record.atom2),
+                        RDC_record.value, 1.000,  1.00
+                    )
+                )
+
+        pales_dummy.close()
+
+        outfile = open("pales.out", 'a')
+        DEVNULL = open(os.devnull, 'w')
+
+        print("call Pales on model: " +
+              str(o + 1) + '/' + str(len(pdb_files)), end="\r")
+        sys.stdout.flush()
+
+        try:
+            if SVD_enable:                          # if SVD is enabled
+                p = subprocess.Popen(
+                    [
+                        csx_obj.ThirdParty.pales,
+                        "-inD", "pales_dummy.txt",
+                        "-pdb", pdb_file,           # pdb file
+                        '-' + lc_model,             # rdc lc model
+                        "-bestFit"                  # SVD
+                    ],
+                    stdout=outfile,
+                    stderr=DEVNULL
+                )
+                p.wait()                        # now wait
+            else:                               # if SVD is disabled (default)
+                subprocess.call(
+                    [
+                        csx_obj.ThirdParty.pales,
+                        "-inD", "pales_dummy.txt",
+                        "-pdb", pdb_file,           # pdb file
+                        '-' + lc_model              # rdc lc model
+                    ],
+                    stdout=outfile,
+                    stderr=DEVNULL
+                )
+        except OSError as e:
+            print("Execution failed:", e, file=sys.stderr)
+        outfile.close()
+        DEVNULL.close()
+
+    print()
+    os.chdir(pwd)
 
 
 def average_pales_output(pales_out, my_RDC_type):
@@ -84,8 +215,9 @@ def rdc(my_CSV_buffer, RDC_lists, pdb_models, my_path, SVD_enabled, lc_model):
         rdc_calced_data[list_num+1] = []
 
         # Pales call, results output file "pales.out"
-        csx_func.callPalesOn(my_path, pdb_models, RDC_dict,
-                             lc_model, SVD_enabled)
+        callPalesOn(
+            my_path, pdb_models, RDC_dict, lc_model, SVD_enabled
+        )
 
         for RDC_type in sorted(list(RDC_dict.keys())):
             print("RDC list", list_num + 1, RDC_type)
