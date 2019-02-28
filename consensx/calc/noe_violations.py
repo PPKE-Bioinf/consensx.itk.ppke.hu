6db8a75bca2c6c1173b77f378b39bea1b461b04e
puinenveturi <daniel.dudola@gmail.com>
2019-02-28 01:26:52 +0100

    refactors NOE restraint record storage

--- consensx/calc/noe_violations.py
+++ consensx/calc/noe_violations.py
@@ -57,99 +57,13 @@ def pdb2coords(model_data):
     return PDB_coords
 
 
-def get_noe_from_file(NOE_file):
-    NOE_key = "_Gen_dist_constraint_list.Constraint_type"
-    NOE_value = "NOE"
-    in_frame = False
-    in_loop = False
-    loops = []
-    NOE_data = []
-
-    for line in open(NOE_file, encoding="utf-8"):
-        tok = line.strip().split()
-
-        if not tok:
-            continue
-
-        if tok[0] == NOE_key and tok[1] == NOE_value:
-            in_frame = True
-            continue
-
-        if in_frame and tok[0] == "save_":
-            # break
-            in_frame = False
-
-        if tok[0] == "loop_":
-            loop_keys = []
-            loop_data = []
-            in_loop = True
-            continue
-
-        if in_loop is True and in_frame is True and tok[0] == "stop_":
-            in_loop = False
-            loops.append([loop_keys, loop_data])
-
-        if in_loop:
-            if tok[0].startswith('_'):
-                loop_keys.append(tok[0])
-            else:
-                loop_data.append(tok)
-
-    for loop in loops:
-        try:
-            ind_ID = loop[0].index("_Gen_dist_constraint.ID")
-            ind_seg1 = loop[0].index("_Gen_dist_constraint.PDB_residue_no_1")
-            ind_seg2 = loop[0].index("_Gen_dist_constraint.PDB_residue_no_2")
-            ind_comp1 = loop[0].index("_Gen_dist_constraint.Comp_ID_1")
-            ind_comp2 = loop[0].index("_Gen_dist_constraint.Comp_ID_2")
-            ind_atom1 = loop[0].index("_Gen_dist_constraint.Atom_ID_1")
-            ind_atom2 = loop[0].index("_Gen_dist_constraint.Atom_ID_2")
-            ind_bnd = loop[0].index(
-                "_Gen_dist_constraint.Distance_upper_bound_val")
-        except ValueError:
-            continue
-
-        for data in loop[1]:
-            NOE_data.append([
-                data[ind_ID],
-                data[ind_seg1], data[ind_seg2],
-                data[ind_comp1], data[ind_comp2],
-                data[ind_atom1], data[ind_atom2],
-                data[ind_bnd]
-            ])
-
-    return NOE_data
-
-
-def noe_violations(model_data, my_path, db_entry, bme_weights=None):
+def noe_violations(model_data, my_path, db_entry, noe_restraints, bme_weights):
     """Back calculate NOE distance violations from given RDC lists and PDB
     models"""
 
     r3_averaging = db_entry.r3average
 
-    # empty class variables
-    csx_obj.Restraint_Record.all_restraints = []
-    csx_obj.Restraint_Record.resolved_restraints = []
-
-    noe_name = db_entry.NOE_file
-    noe_file_path = my_path + noe_name
-    save_shifts = get_noe_from_file(noe_file_path)
-    noe_n = save_shifts[-1][0] + " distance restraints found"
-
-    csx_id = 1
-    prev_id = 1
-    for data in save_shifts:
-        csx_obj.Restraint_Record(
-            csx_id, data[0], data[1], data[2], data[3],
-            data[4], data[5], data[6], data[7]
-        )
-
-        if prev_id != data[0]:
-            prev_id = data[0]
-            csx_id += 1
-
-    # fetch all restraint from class
-    restraints = csx_obj.Restraint_Record.getNOERestraints(model_data)
+    restraints = noe_restraints.resolved_restraints
 
     PDB_coords = pdb2coords(model_data)
     prev_id = -1
@@ -163,11 +77,11 @@ def noe_violations(model_data, my_path, db_entry, bme_weights=None):
         all_distances[model] = {}
 
         for restraint_num, restraint in enumerate(restraints):
-            rest_id = int(restraint.csx_id)
-            resnum1 = restraint.seq_ID1
-            atom1 = restraint.atom_ID1
-            resnum2 = restraint.seq_ID2
-            atom2 = restraint.atom_ID2
+            rest_id = int(restraint["csx_id"])
+            resnum1 = restraint["seq_ID1"]
+            atom1 = restraint["atom_ID1"]
+            resnum2 = restraint["seq_ID2"]
+            atom2 = restraint["atom_ID2"]
 
             atom_coord1 = PDB_coords[model][resnum1][atom1]
             atom_coord2 = PDB_coords[model][resnum2][atom2]
@@ -182,16 +96,16 @@ def noe_violations(model_data, my_path, db_entry, bme_weights=None):
             else:
                 prev_id = rest_id
                 avg_distances[model][rest_id] = []
-                str_distaces[rest_id] = restraint.dist_max
+                str_distaces[rest_id] = restraint["dist_max"]
 
                 avg_distances[model][rest_id].append(distance)
 
     for restraint_num, restraint in enumerate(restraints):
-        rest_id = int(restraint.csx_id)
-        resnum1 = restraint.seq_ID1
-        atom1 = restraint.atom_ID1
-        resnum2 = restraint.seq_ID2
-        atom2 = restraint.atom_ID2
+        rest_id = int(restraint["csx_id"])
+        resnum1 = restraint["seq_ID1"]
+        atom1 = restraint["atom_ID1"]
+        resnum2 = restraint["seq_ID2"]
+        atom2 = restraint["atom_ID2"]
 
         dist_str = "> {} {} {} {} {}  |   ".format(
             rest_id, resnum1, atom1, resnum2, atom2
@@ -209,7 +123,7 @@ def noe_violations(model_data, my_path, db_entry, bme_weights=None):
 
     for model in list(PDB_coords.keys()):
         for restraint in restraints:
-            curr_id = int(restraint.csx_id)
+            curr_id = int(restraint["csx_id"])
 
             if prev_id == curr_id:
                 continue
@@ -235,7 +149,7 @@ def noe_violations(model_data, my_path, db_entry, bme_weights=None):
     # "csx_id" number. Averaging is done on "in GROUP" distances
 
     for restraint in restraints:
-        curr_id = int(restraint.curr_distID)
+        curr_id = int(restraint["curr_distID"])
         avg = 0.0
 
         if bme_weights:
@@ -261,14 +175,14 @@ def noe_violations(model_data, my_path, db_entry, bme_weights=None):
         prev_id = -1
 
         for restraint in restraints:
-            if prev_id == restraint.csx_id:
+            if prev_id == restraint["csx_id"]:
                 continue
 
-            prev_id = restraint.csx_id
+            prev_id = restraint["csx_id"]
 
             exp_dat_file.write(
-                str(restraint.csx_id) + "\t" +
-                str(restraint.dist_max) + "\t0.1\n"
+                str(restraint["csx_id"]) + "\t" +
+                str(restraint["dist_max"]) + "\t0.1\n"
             )
 
     with open(my_path + bme_calc_filename, "w") as calc_dat_file:
@@ -313,4 +227,4 @@ def noe_violations(model_data, my_path, db_entry, bme_weights=None):
     print("Total # of violations:", viol_count)
     make_noe_hist(my_path, violations)
 
-    return noe_n, viol_count
+    return viol_count
