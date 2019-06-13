09fefa405e4ca298b64e3eea72722a8747639d70
puinenveturi <daniel.dudola@gmail.com>
2019-06-13 13:30:04 +0200

    makes average correlation per model value visible in graph #35

--- consensx/graph/mod_correl_graph.py
+++ consensx/graph/mod_correl_graph.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-plt.switch_backend('Agg')
+plt.switch_backend("Agg")
 
 
 def mod_correl_graph(my_path, correl, avg_corr, model_corrs, corr_graph_name):
@@ -13,24 +13,36 @@ def mod_correl_graph(my_path, correl, avg_corr, model_corrs, corr_graph_name):
     plt.figure(figsize=(6, 5), dpi=80)
 
     plt.plot(
-        list(range(0, len(model_corrs))), [correl] * len(model_corrs),
-        linewidth=2.0, color='green', label='Ensemble corr.', alpha=.7
+        list(range(0, len(model_corrs))),
+        [correl] * len(model_corrs),
+        linewidth=2.0,
+        color="green",
+        label="Ensemble corr. ({:.4f})".format(correl),
+        alpha=0.7,
     )
 
     plt.plot(
-        list(range(0, len(model_corrs))), [avg_corr] * len(model_corrs),
-        linewidth=2.0, color='red', label='Avg. corr. per model', alpha=.7
+        list(range(0, len(model_corrs))),
+        [avg_corr] * len(model_corrs),
+        linewidth=2.0,
+        color="red",
+        label="Avg. corr. per model ({:.4f})".format(avg_corr),
+        alpha=0.7,
     )
 
     plt.plot(
-        list(range(0, len(model_corrs))), sorted(model_corrs),
-        linewidth=2.0, color='blue', label='Corr. per model', alpha=.7
+        list(range(0, len(model_corrs))),
+        sorted(model_corrs),
+        linewidth=2.0,
+        color="blue",
+        label="Corr. per model",
+        alpha=0.7,
     )
 
-    plt.legend(loc='lower left')
+    plt.legend(loc="lower left")
     plt.axis([-1, len(model_corrs), 0, 1])
-    plt.xlabel('models (worse to best)')
-    plt.ylabel('correlation')
+    plt.xlabel("models (worse to best)")
+    plt.ylabel("correlation")
     plt.grid(axis="y")
     plt.tight_layout(pad=1.08)
     plt.savefig(my_path + "/" + corr_graph_name, format="svg")
